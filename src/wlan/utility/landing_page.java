df86ac36f5208f67a5a5bfdfebeba3219616e683
CodyNeeraj <neerajkumar332424@gmail.com>
2020-11-19 16:41:07 +0530

    Started V1.1 since, Driver code class removed and core buttons merged in main class as functions,add[ing] wlan_start command procedure completely, minor ui improvents,code formatting

--- src/wlan/utility/landing_page.java
+++ src/wlan/utility/landing_page.java
@@ -2,12 +2,13 @@ package wlan.utility;
 
 import java.awt.Color;
 import java.awt.Desktop;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.UnsupportedLookAndFeelException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,7 +22,6 @@ import javax.swing.UnsupportedLookAndFeelException;
  */
 public class landing_page extends javax.swing.JFrame 
 {
-
     /**
      * Creates new form landing_page
      */
@@ -32,7 +32,85 @@ public class landing_page extends javax.swing.JFrame
     
     String ssid_name;
     String ssid_pass;
+    String stop_cmd = "netsh wlan stop hostednetwork";
+    String restart_cmd  = "netsh wlan stop hostednetwork && netsh wlan start hostednetwork";
+    String start_cmd;
+    
+    
+    
+    public void stop_func()
+   {
+        try 
+        {
+            Process process = Runtime.getRuntime().exec(stop_cmd);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+                while((line = reader.readLine()) != null)
+                {
+                    status_field.append(line);
+                    System.out.println(line);
+                }
+         } 
+        catch (IOException e) 
+        {}
+   }
 		
+    public void restart_func()
+   {
+    try 
+        {
+           Process process = Runtime.getRuntime().exec(restart_cmd);
+           BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+           String line;
+           while ((line = reader.readLine()) != null)
+            {
+                status_field.setText(line);
+                System.out.println(line);
+            }
+         } 
+        catch (IOException e) 
+        {}
+   }
+    
+    public void reset_func()
+   {
+    try 
+        {
+            Process process = Runtime.getRuntime().exec(stop_cmd);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                status_field.append(line);
+                System.out.println(line);
+            }
+        } 
+        catch (IOException e) 
+        {}
+   }
+    
+     public void start_func()
+   {
+       String predefined_cmd = "netsh wlan set hostednetwork mode=allow ssid=";
+       StringBuilder final_cmd = new StringBuilder();
+       final_cmd.append(predefined_cmd).append(ssid_name).append(" key=").append(ssid_pass);
+       start_cmd = final_cmd.toString();
+        try 
+        {
+            Process process = Runtime.getRuntime().exec(stop_cmd);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+                while((line = reader.readLine()) != null)
+                {
+                    status_field.append(line);
+                    System.out.println(line);
+                }
+                status_field.setText(start_cmd);
+                System.out.println(start_cmd);
+         } 
+        catch (IOException e) 
+        {}
+   }
 	
 	
     /**
@@ -154,11 +232,13 @@ public class landing_page extends javax.swing.JFrame
             }
         });
 
+        status_field.setEditable(false);
         status_field.setColumns(10);
         status_field.setFont(new java.awt.Font("Arial", 0, 10)); // NOI18N
-        status_field.setLineWrap(true);
         status_field.setRows(100);
         status_field.setToolTipText("Drag Useful info from here using Copy & Paste");
+        status_field.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
+        status_field.setDragEnabled(true);
         jScrollPane1.setViewportView(status_field);
 
         ssid_clr_btn.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
@@ -204,6 +284,11 @@ public class landing_page extends javax.swing.JFrame
 
         jMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_MASK));
         jMenuItem3.setText("Reset Everthing");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem3);
 
         jMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Q, java.awt.event.InputEvent.CTRL_MASK));
@@ -257,42 +342,42 @@ public class landing_page extends javax.swing.JFrame
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(17, 17, 17)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(17, 17, 17)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
                             .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(ssid_clr_btn)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(pass_checkbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(pass_clr_btn))
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(jLabel3)
+                                    .addGap(18, 18, 18)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addComponent(pass_field, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(ssid_field, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                         .addGap(17, 17, 17)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(ssid_clr_btn)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addGroup(layout.createSequentialGroup()
-                                            .addComponent(pass_checkbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(pass_clr_btn))
-                                        .addGroup(layout.createSequentialGroup()
-                                            .addComponent(jLabel3)
-                                            .addGap(18, 18, 18)
-                                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                                .addComponent(pass_field, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addComponent(ssid_field, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                                .addGap(17, 17, 17)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(start_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(stop_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(restart_btn)
-                                    .addComponent(reset_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addComponent(jScrollPane1))))
+                            .addComponent(start_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(stop_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(restart_btn)
+                            .addComponent(reset_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(17, 17, 17))
             .addGroup(layout.createSequentialGroup()
                 .addGap(135, 135, 135)
                 .addComponent(jLabel1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -327,9 +412,9 @@ public class landing_page extends javax.swing.JFrame
                         .addComponent(reset_btn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(pass_checkbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(12, 12, 12)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(17, 17, 17))
+                .addContainerGap())
         );
 
         pack();
@@ -343,6 +428,7 @@ public class landing_page extends javax.swing.JFrame
         }
         else
         {
+            ssid_pass = null;
             pass_field.setEnabled(false);
             pass_clr_btn.setEnabled(false);
         }
@@ -353,13 +439,12 @@ public class landing_page extends javax.swing.JFrame
     }//GEN-LAST:event_pass_fieldActionPerformed
 
     private void start_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_start_btnActionPerformed
-        ssid_name  = ssid_field.getText();
-        ssid_pass = new String(pass_field.getPassword());
-        status_field.setForeground(Color.black);
-        status_field.setText(""); // fault happening at last phase of starting the wlan
+    ssid_pass = new String(pass_field.getPassword());
+    status_field.setForeground(Color.black);
+    //status_field.setText("here am i"); // fault happening at last phase of starting the wlan
        // status_field.setText("Information filled sucessfully..");
-          
-        if(ssid_name.isEmpty())
+       
+        if(ssid_field.getText().isEmpty())
         {
             status_field.setText("Enter SSID name...");
             ssid_name  = ssid_field.getText();
@@ -372,49 +457,70 @@ public class landing_page extends javax.swing.JFrame
                      status_field.setText("Please Enter SSID name and Password..");
                      ssid_name  = ssid_field.getText();
                      ssid_pass = new String(pass_field.getPassword());
+                     start_func();
                     }
                }
+        }
+        
+        else if(!pass_checkbox.isSelected())
+        {
+            ssid_pass = "default (since not selected)";
         }
         
         else if(pass_checkbox.isSelected())
             {
                 if(ssid_pass.isEmpty())
                     {
-                     status_field.append("\nPlease enter Password");
+                     status_field.append("\nPassword can't be left empty !!!");
                      status_field.setForeground(Color.red);
                      ssid_pass = new String(pass_field.getPassword());
                     }
+                start_func();
             }
         else
         {
             status_field.setForeground(Color.black);
             status_field.setText("everything seems done..");
-        }
-       
-        //your driver code for runtime goes here.......
+            start_func();
+        }   
     }//GEN-LAST:event_start_btnActionPerformed
 
     private void reset_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reset_btnActionPerformed
-       
        //resetting/clearing all fields 
-        
        status_field.setText("");
        ssid_field.setText("");
        pass_field.setText("");
-       
-       //driver class reset function called
-       driver_code reset = new driver_code();
-       reset.reset_func();
+       reset_func();  
     }//GEN-LAST:event_reset_btnActionPerformed
 
     private void stop_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_stop_btnActionPerformed
-       driver_code stop = new driver_code();
+       /*driver_code stop = new driver_code();
        stop.stop_func();
+       // as the above class was there in version 1.0
+       
+        try 
+        {
+            Process process = Runtime.getRuntime().exec(stop_cmd);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) 
+            {
+                String line;
+                while ((line = reader.readLine()) != null)
+                {
+                    status_field.setText(line);
+                    System.out.println(line);
+                }
+            }
+        } 
+       
+        catch (IOException e) 
+        {
+            
+        }     */
+       stop_func();
     }//GEN-LAST:event_stop_btnActionPerformed
 
     private void ssid_clr_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ssid_clr_btnActionPerformed
-        ssid_field.setText("");   //will clear the ssid field
-        
+        ssid_field.setText("");   //will clear the ssid field   
     }//GEN-LAST:event_ssid_clr_btnActionPerformed
 
     private void ssid_fieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ssid_fieldActionPerformed
@@ -476,26 +582,32 @@ public class landing_page extends javax.swing.JFrame
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem9ActionPerformed
-      
-        
-            try {
-            Runtime.getRuntime().exec(new String[] {"cmd","/k","start","netsh", "wlan" ,"show" ,"drivers"});
-            } catch (IOException ex) {
+      try
+      {
+          
+          
+        Runtime.getRuntime().exec(new String[] {"cmd","/c","start","netsh", "wlan" ,"show" ,"drivers"});
+            
+            
+              
+          // can be used in this way JVM garbage collector is best in random objects
+          // String cmd = "cmd.exe /c start netsh wlan show drivers"; 
+        } 
+      catch (IOException ex)
+         {
             Logger.getLogger(landing_page.class.getName()).log(Level.SEVERE, null, ex);
-            }
-                 
-            // can be used in this way JVM garbage collector is best in random objects  
-             // String cmd = "cmd.exe /c start netsh wlan show drivers"; 
-        
+        }
+           
     }//GEN-LAST:event_jMenuItem9ActionPerformed
 
     private void restart_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_restart_btnActionPerformed
        
-        //object of driver class
-        
-        driver_code restart = new driver_code();
-        restart.restart_func();
+       restart_func();
     }//GEN-LAST:event_restart_btnActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -509,17 +621,16 @@ public class landing_page extends javax.swing.JFrame
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if("Windows".equals(info.getName()))
-                {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
-                } 
-             
+                }
             }
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException ex) 
-        {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(landing_page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        
         //</editor-fold>
         
         //</editor-fold>
@@ -556,7 +667,7 @@ public class landing_page extends javax.swing.JFrame
     private javax.swing.JButton ssid_clr_btn;
     private javax.swing.JTextField ssid_field;
     private javax.swing.JButton start_btn;
-    public static javax.swing.JTextArea status_field;
+    private javax.swing.JTextArea status_field;
     private javax.swing.JButton stop_btn;
     // End of variables declaration//GEN-END:variables
 }
