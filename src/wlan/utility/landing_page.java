b7f1ef00510b8e930904bcb559ec4d7b9092a13a
CodyNeeraj <neerajkumar332424@gmail.com>
2020-11-21 15:47:49 +0530

    added feedback button, removed splahframe

--- src/wlan/utility/landing_page.java
+++ src/wlan/utility/landing_page.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /*
  * The MIT License
@@ -195,6 +196,7 @@ public class landing_page extends javax.swing.JFrame
         jMenuItem6 = new javax.swing.JMenuItem();
         jMenuItem7 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem8 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -368,6 +370,14 @@ public class landing_page extends javax.swing.JFrame
 
         jMenuItem5.setText("Developer");
         jMenu2.add(jMenuItem5);
+
+        jMenuItem1.setText("Feedback");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem1);
 
         jMenuItem8.setText("About");
         jMenuItem8.addActionListener(new java.awt.event.ActionListener() {
@@ -640,17 +650,18 @@ public class landing_page extends javax.swing.JFrame
                        output.append(line);
                    }               
                 para = output.toString();
-                new drivercheck_splashscreen(para).setVisible(true);
+                //new drivercheck_splashscreen(para).setVisible(true);
                 
                 reader.close();
                  
                if(para.length()>=55)
                 {
-                    status_field.setText("ahaa gal baaat");
+                   JOptionPane.showMessageDialog(rootPane,"All Drivers Are Available !","Result", JOptionPane.INFORMATION_MESSAGE);
                 }
                else
                {
-                   status_field.setText("ma chudao heni chlana taade system ch .......");
+                   JOptionPane.showConfirmDialog(rootPane,"No Drivers Found..\nStill want to use without drivers ?..  ", "Error" , JOptionPane.OK_CANCEL_OPTION);
+                   
                }
       }
       catch (IOException ex)
@@ -692,6 +703,10 @@ public class landing_page extends javax.swing.JFrame
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -736,6 +751,7 @@ public class landing_page extends javax.swing.JFrame
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu5;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
